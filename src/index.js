b3dde436b49cf7ca337f3df3c02ae268644c12b0
Sara <sara@local>
2024-01-04 17:52:50 +0330

    now start

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import One from './One/One'
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <One />
+    <App />
   </React.StrictMode>
 );
 
