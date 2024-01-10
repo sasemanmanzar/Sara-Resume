127e8e1223e326517823b6a610c2451178a3672d
Sara <sara@local>
2024-01-10 19:07:01 +0330

    ok pages

--- src/Pages/Two/Two.js
+++ src/Pages/Two/Two.js
@@ -3,12 +3,9 @@ import './Two.css'
 function Two(){
 
     return(
-        <>
         <div id='IdTwo' className='two'>
             <div>page 2</div>
-            <button>dd</button>
         </div>
-        </>
     );
 }
 
