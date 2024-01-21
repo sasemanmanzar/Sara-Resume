3f107efc7bb2875b32cc52d50236e8a2bdee38ab
Sara <sara@local>
2024-01-21 12:55:20 +0330

    page one ok

--- src/Pages/One/One.js
+++ src/Pages/One/One.js
@@ -2,8 +2,9 @@
 // import Two from '../Two/Two'
 // import { useCallback } from 'react';
 
-import './One.css'
-import picSara from '../../Pic/sara2.jpg'
+import './One.css';
+import picSara from '../../Pic/sara2.jpg';
+// import App from '../../App';
 
 function One(){
 
@@ -11,16 +12,51 @@ function One(){
     //     alert("Hi!");
     // }
 
+    const scrollToMarginPage1 = () => {
+        const marginDot1 = document.getElementById('IdOne'); 
+          if (marginDot1) {
+            marginDot1.scrollIntoView();
+          }
+      };
+
+      const scrollToMarginPage2 = () => {
+        const marginDot2 = document.getElementById('IdTwo'); 
+          if (marginDot2) {
+            marginDot2.scrollIntoView();
+          }
+      };
+
+      const scrollToMarginPage3 = () => {
+        const marginDot3 = document.getElementById('IdThree'); 
+          if (marginDot3) {
+            marginDot3.scrollIntoView();
+          }
+      };
+    
+      const scrollToMarginPage4 = () => {
+        const marginDot4 = document.getElementById('IdFour'); 
+          if (marginDot4) {
+            marginDot4.scrollIntoView();
+          }
+      };
+    
+      const scrollToMarginPage5 = () => {
+        const marginDot5 = document.getElementById('IdFive'); 
+          if (marginDot5) {
+            marginDot5.scrollIntoView();
+          }
+      };
+
     return(
         <div id='IdOne' className='one'>
              <img className='picSara' src={picSara} alt="" />
              <div className='textPageOne'>سارا آسمان منظر</div>
              
-             <div className='circlePageOne circlePageOne1'><p>تماس با ما</p></div>
-             <div className='circlePageOne circlePageOne2'><p>تجارب</p></div>
-             <div className='circlePageOne circlePageOne3'><p>مهارت ها</p></div>
-             <div className='circlePageOne circlePageOne4'><p>مقالات</p></div>
-             <div className='circlePageOne circlePageOne5'><p>سوابق تحصیلی</p></div>
+             <div className='circlePageOne circlePageOne1' onClick={scrollToMarginPage5}><p>تماس با ما</p></div>
+             <div className='circlePageOne circlePageOne2' onClick={scrollToMarginPage4}><p>تجارب</p></div>
+             <div className='circlePageOne circlePageOne3' onClick={scrollToMarginPage3}><p>مهارت ها</p></div>
+             <div className='circlePageOne circlePageOne4' onClick={scrollToMarginPage2}><p>مقالات</p></div>
+             <div className='circlePageOne circlePageOne5' onClick={scrollToMarginPage1}><p>سوابق تحصیلی</p></div>
 
         </div>
     );
