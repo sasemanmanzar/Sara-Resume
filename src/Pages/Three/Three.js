3b98b1cfa042012e0aeff0e100db59f944cc2f44
Sara <sara@local>
2024-01-24 18:21:47 +0330

    page 3

--- src/Pages/Three/Three.js
+++ src/Pages/Three/Three.js
@@ -3,9 +3,12 @@ import './Three.css'
 function Three(){
 
     return(
-        <>
-        <div id='IdThree' className='three'>Page 3</div>
-        </>
+        <div id='IdThree' className='three'>
+        <div className='smallBoxPaper topPaper1'><p>مقالات</p></div>
+        <div className='boxPaper topPaper2'>
+            <p>طراحی بستر نرم افزاری تحلیل داده کاربران در بازی ها و طبقه بندی شاخص های ضروری</p>
+        </div>
+        </div>
     );
 }
 
