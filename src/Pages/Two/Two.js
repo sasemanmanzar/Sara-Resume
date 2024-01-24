a9844acc3f4e38ea40162c709b4aed3df879dedb
Sara <sara@local>
2024-01-24 07:50:26 +0330

    page 2

--- src/Pages/Two/Two.js
+++ src/Pages/Two/Two.js
@@ -4,7 +4,12 @@ function Two(){
 
     return(
         <div id='IdTwo' className='two'>
-            <div>page 2</div>
+            <div className='smallBox1 top1'>سوابق تحصیلی</div>
+            <div className='smallBox top2'>کارشناسی ارشد</div>
+            <div className='box top3'>قم</div>
+            <div className='smallBox top4'>کارشناسی</div>
+            <div className='box top5'>خواجه</div>
+
         </div>
     );
 }
