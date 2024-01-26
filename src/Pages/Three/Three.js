71955c834ddde757cdd086dce53b4ffc86e5fb71
Sara <sara@local>
2024-01-27 00:28:10 +0330

    page 3

--- src/Pages/Three/Three.js
+++ src/Pages/Three/Three.js
@@ -1,47 +1,63 @@
 import './Three.css'
 
+const newTab = () => {
+    window.open(this.href);
+    return true;
+};
+
+const afterPress = () => {
+    window.open(this.href);
+    return true;
+}
+
 function Three(){
 
     return(
         <div id='IdThree' className='three'>
         <div className='smallBoxPaper topPaper1'><p>مقالات</p></div>
         <div className='boxPaper topPaper2'>
-            <p>طراحی بستر نرم افزاری تحلیل داده کاربران در بازی ها و طبقه بندی شاخص های ضروری</p>
+            <p id='title'>طراحی بستر نرم افزاری تحلیل داده کاربران در بازی ها و طبقه بندی شاخص های ضروری</p>
             <p>محمد محسن آسمان منظر، سارا آسمان منظر، بهروز مینایی</p>
             <p>کنفرانس تحقیقات بازی های دیجیتال، گرایش ها، فناوری ها و کاربرد ها</p>
             <p>دوم و سوم آذر ماه سال ۱۳۹۶، دانشگاه علم و صنعت، تهران</p>
-            <p>https://civilica.com/doc/696919</p>
+            <p><a href="https://civilica.com/doc/696919" target="_blank">https://civilica.com/doc/696919</a></p>
+            <hr/>
+            <p id='title'>بررسی فرآیند داده کاوی و طبقه بندی راهکارهای حل مشکلات در مرحله پیش پردازش داده ها</p>
+            <p>سارا آسمان منظر</p>
+            <p> کنفرانس بین المللی پژوهش در مهندسی، علوم و تکنولوژی</p>
+            <p>سی ام تیر ۹۴ استانبول ترکیه</p>
+            <p>https://civilica.com/doc/398506</p>
+            <hr/>
+            <p id='title'>بیان و استخراج نیازمندی های امنیتی با استفاده از بهترین روش های موجود</p>
+            <p>سارا آسمان منظر، محدثه سادات قماشی</p>
+            <p> همایش ملی الکترونیکی دستاوردهای نوین در علوم مهندسی و پایه</p>
+            <p>هشتم و نهم مرداد ماه سال ۱۳۹۳</p>
+            <p>https://civilica.com/doc/304119</p>
             <hr/>
             <p></p>
             <p></p>
             <p></p>
             <p></p>
             <p></p>
+            <hr/>
             <p></p>
             <p></p>
             <p></p>
             <p></p>
             <p></p>
+            <hr/>
             <p></p>
             <p></p>
             <p></p>
             <p></p>
             <p></p>
+            <hr/>
             <p></p>
             <p></p>
             <p></p>
             <p></p>
             <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
+            <hr/>
             <p></p>
             <p></p>
             <p></p>
