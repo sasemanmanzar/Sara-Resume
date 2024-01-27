8f10dc257011beab6083ca18ff39e30b6f0a4267
Sara <sara@local>
2024-01-27 17:49:33 +0330

    page 4

--- src/Pages/Four/Four.js
+++ src/Pages/Four/Four.js
@@ -7,8 +7,49 @@ function Four(){
     return(
         <div id='IdFour' className='four'>
             <div className='smallBoxSkill topSkill1'><p>مهارت ها</p></div>
+            <table>
+                {/* <tr>
+                    <th>زبان خارجه</th>
+                    <th></th>
+                    <th>حوزه کامپیوتر</th>
+                </tr> */}
+                <tr>
+                    <td>Data Mining</td>
+                    <td></td>
+                    <td>React.Js</td>
+                </tr>
+                <tr>
+                    <td>Android Development</td>
+                    <td></td>
+                    <td>Research</td>
+                </tr>
+                <tr>
+                    <td>Front-end Development</td>
+                    <td></td>
+                    <td>Persian language</td>
+                </tr>
+                <tr>
+                    <td>Cascading Style Sheets (CSS)</td>
+                    <td></td>
+                    <td>English language</td>
+                </tr>
+                <tr>
+                    <td>Java Script</td>
+                    <td></td>
+                    <td>German language</td>
+                </tr>
+            </table>
         </div>
     );
 }
 
 export default Four;
+
+
+// Data Mining
+// Android Development
+// Front-end Development
+// Cascading Style Sheets (CSS)
+// Java Script
+// React.Js
+// Research
