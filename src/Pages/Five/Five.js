97e04e1cb36ddcf41de837b9ca60143dcaab029a
Sara <sara@local>
2024-01-28 00:29:11 +0330

    page 5

--- src/Pages/Five/Five.js
+++ src/Pages/Five/Five.js
@@ -4,7 +4,18 @@ function Five(){
 
     return(
         <div id='IdFive' className='five'>
+            <div className='smallBoxExp topExp1'><p>تجارب</p></div>
 
+            <div className='boxExp topExp2'>
+            <p id="">برنامه نقاشی مداد آبی، زبان پروژه اندروید، منتشرشده در کافه بازار</p>
+            <p id="">تعداد نصب +۳۰۰۰</p>
+            <p id="">برنامه نقاشی مخصوص کودکان که دارای ویژگی های خلاقانه و جذاب است. این برنامه شامل نقاشی، رنگ آمیزی تصاویر، وصل کردن نقطه چین ها و امکاناتی از قبیل ذخیره تصاویر و پرداخت درون برنامه ای است.</p>
+            <p id="">آخرین به روز رسانی، انتشار نسخه دو و دو در تاریخ ۹ تیر ۱۳۹۹</p>
+            <p id="">انتشار نسخه دو و یک در تاریخ ۱۲ خرداد ۱۳۹۹</p>
+            <p id="">انتشار نسخه یک در تاریخ ۱۶ دی ۱۳۹۸</p>
+            <p id=""><a href="https://cafebazaar.ir/app/com.example.BluePencil" target="_blank" rel="noreferrer">caffee</a></p>
+            <hr/>
+            </div>
         </div>
     );
 }
