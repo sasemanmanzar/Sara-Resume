34db45cbfdc6e28ba10a82219de0bc51d1e69862
Sara <sara@local>
2024-01-27 23:42:11 +0330

    page 4

--- src/Pages/Four/Four.js
+++ src/Pages/Four/Four.js
@@ -14,29 +14,54 @@ function Four(){
                     <th>حوزه کامپیوتر</th>
                 </tr> */}
                 <tr>
-                    <td>Data Mining</td>
-                    <td></td>
-                    <td>React.Js</td>
+                    <td id="s1">Data Mining</td>
+                    <td id="s2"></td>
+                    <td id="s3">React.Js</td>
                 </tr>
                 <tr>
-                    <td>Android Development</td>
-                    <td></td>
-                    <td>Research</td>
+                    <td id="sk1"></td>
+                    <td id="sk2"></td>
+                    <td id="sk3"></td>
                 </tr>
                 <tr>
-                    <td>Front-end Development</td>
-                    <td></td>
-                    <td>Persian language</td>
+                    <td id="s01">Android Development</td>
+                    <td id="s02"></td>
+                    <td id="s03">Research</td>
                 </tr>
                 <tr>
-                    <td>Cascading Style Sheets (CSS)</td>
-                    <td></td>
-                    <td>English language</td>
+                    <td id="sk01"></td>
+                    <td id="sk02"></td>
+                    <td id="sk03"></td>
                 </tr>
                 <tr>
-                    <td>Java Script</td>
-                    <td></td>
-                    <td>German language</td>
+                    <td id="s001">Front-end Development</td>
+                    <td id="s002"></td>
+                    <td id="s003">Persian language</td>
+                </tr>
+                <tr>
+                    <td id="sk001"></td>
+                    <td id="sk002"></td>
+                    <td id="sk003"></td>
+                </tr>
+                <tr>
+                    <td id="s0001">Cascading Style Sheets (CSS)</td>
+                    <td id="s0002"></td>
+                    <td id="s0003">English language</td>
+                </tr>
+                <tr>
+                    <td id="sk0001"></td>
+                    <td id="sk0002"></td>
+                    <td id="sk0003"></td>
+                </tr>
+                <tr>
+                    <td id="s00001">Java Script</td>
+                    <td id="s00002"></td>
+                    <td id="s00003">German language</td>
+                </tr>
+                <tr>
+                    <td id="sk00001"></td>
+                    <td id="sk00002"></td>
+                    <td id="sk00003"></td>
                 </tr>
             </table>
         </div>
