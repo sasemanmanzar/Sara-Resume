be7494b9cc535d70790294d77e9a1ae0feac55c3
Sara <sara@local>
2024-01-30 19:31:42 +0330

    pg 6 Email

--- src/Pages/Six/Six.js
+++ src/Pages/Six/Six.js
@@ -1,12 +1,37 @@
 import './Six.css';
+import emailjs from 'emailjs-com';
+
 
 function Six(){
+
+    // function sendEmail(e) {
+    //     e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+    
+    //     emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+    //       .then((result) => {
+    //           window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+    //       }, (error) => {
+    //           console.log(error.text);
+    //       });
+    //   }
+
+    function sendEmail(e) {
+        e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+    
+        emailjs.sendForm('service_y8ycm1s', 'template_3gx9e0d', e.target, '9U64idXn07ZppBH-Q')
+          .then((result) => {
+              window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+          }, (error) => {
+              console.log(error.text);
+          });
+      }
+
 
     return(
         <div id='IdSix' className='six'>
             <div className='smallBoxCallMe topCallMe1'><p>تماس با ما</p></div>
 
-            <form action=''>
+            {/* <form action=''>
                 <table className='tableP6'>
                     <tr>
                         <td id="EmailP6"><input type="text" name="EmailP6" value="ایمیل"/></td>
@@ -22,7 +47,26 @@ function Six(){
                         <td id="SubmitP6" colspan="2"><input type="submit" name="SubmitP6" value="ارسال"/></td>
                     </tr>
                 </table>
+            </form> */}
+
+            <form className="contact-form" onSubmit={sendEmail}>
+                <input type="hidden" name="contact_number" />
+                <br/>
+                <label>نام</label>
+                <input type="text" name="from_name" />
+                <br/>
+                <label>ایمیل</label>
+                <input type="email" name="from_email" />
+                <br/>
+                <label>موضوع</label>
+                <input type="text" name="subject" />
+                <br/>
+                <label>متن پیام</label>
+                <textarea name="html_message" />
+                <br/>
+                <input type="submit" value="Send" />
             </form>
+
         </div>
     );
 }
