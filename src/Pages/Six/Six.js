5a5eec7374296ea768d1f5b5d025f67446a99dcc
Sara <sara@local>
2024-01-31 14:48:49 +0330

    page 6

--- src/Pages/Six/Six.js
+++ src/Pages/Six/Six.js
@@ -1,5 +1,6 @@
 import './Six.css';
-import emailjs from 'emailjs-com';
+// import emailjs from 'emailjs-com';
+import { useState } from 'react';
 
 
 function Six(){
@@ -15,34 +16,50 @@ function Six(){
     //       });
     //   }
 
-    function sendEmail(e) {
-        e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+    // function sendEmail(e) {
+    //     e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
     
-        emailjs.sendForm('service_y8ycm1s', 'template_3gx9e0d', e.target, '9U64idXn07ZppBH-Q')
-          .then((result) => {
-              window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
-          }, (error) => {
-              console.log(error.text);
-          });
+    //     emailjs.sendForm('service_y8ycm1s', 'template_3gx9e0d', e.target, '9U64idXn07ZppBH-Q')
+    //       .then((result) => {
+    //           window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+    //       }, (error) => {
+    //           console.log(error.text);
+    //       });
+    //   }
+
+    // function Sname() { const[ nameP6, setName ]= useState("نام"); }
+    
+    
+    const [InfoSendEmail, SetInfo] = useState({
+    Name: "نام",
+    Email: "ایمیل",
+    Title: "موضوع",
+    Text: "متن پیام"
+    });
+
+    const updateInfoSendEmail = () => {
+        SetInfo(previousState => {
+          return { ...previousState, Name: "blue" }
+        });
       }
-
-
 
     return(
         <div id='IdSix' className='six'>
             <div className='smallBoxCallMe topCallMe1'><p>تماس با ما</p></div>
 
+            {/* <div>sara {InfoSendEmail.Email} </div> */}
+
             <form action=''>
                 <table className='tableP6'>
                     <tr>
-                        <td id="EmailP6"><input type="text" name="EmailP6" /></td>
-                        <td id="NameP6"><input type="text" name="NameP6" /></td>
+                        <td id="EmailP6"><input type="text" name="EmailP6" value={InfoSendEmail.Email} /></td>
+                        <td id="NameP6"><input type="text" name="NameP6" value={InfoSendEmail.Name} /></td>
                     </tr>
                     <tr>
-                        <td id="TitleP6" colspan="2"><input type="text" name="TitleP6" /></td>
+                        <td id="TitleP6" colspan="2"><input type="text" name="TitleP6" value={InfoSendEmail.Title}/></td>
                     </tr>
                     <tr>
-                        <td id="TextP6" colspan="2"><textarea type="text" name="TextP6" /></td>
+                        <td id="TextP6" colspan="2"><textarea type="text" name="TextP6" value={InfoSendEmail.Text}/></td>
                     </tr>
                     <tr>
                         <td id="SubmitP6" colspan="2"><input type="submit" name="SubmitP6" value="ارسال"/></td>
