5888b0d2c9519711ae2adb473b4a28baa970fdc2
Sara <sara@local>
2024-02-16 23:21:55 +0330

    p1 width & height

--- src/Pages/One/One.js
+++ src/Pages/One/One.js
@@ -1,6 +1,6 @@
 import './One.css';
 import picSara from '../../Pic/sara.jpg';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 
 function One(){
 
@@ -52,19 +52,36 @@ function One(){
       // const infoClassSara = (isWidthTurningPoint ?  "Rectangle-Large" : "sara_area Rectangle");
 
 
-      var Width1 = document.documentElement.clientWidth;
-      var Width2 = document.body.clientWidth;
-      var Width3 = window.innerWidth;
-      var Width = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
+      // var Width1 = document.documentElement.clientWidth;
+      // var Width2 = document.body.clientWidth;
+      // var Width3 = window.innerWidth;
+      // var Width = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
       // window.screen.width;
 
+      // const sara =  window.innerHeight;
       // const [WhatClientWidth, setClientWidth] = useState('0');
       // setClientWidth({Width});
       // setCar({color: "blue"})
 
+      
+      // useEffect(() => {
+      //   function setClientWidth() { return(WhatClientWidth = window.innerWidth);  } ;
+      // });
+
+        const [width, setWidth]   = useState(window.innerWidth);
+        const [height, setHeight] = useState(window.innerHeight);
+        const updateDimensions = () => {
+            setWidth(window.innerWidth);
+            setHeight(window.innerHeight);
+        }
+        useEffect(() => {
+            window.addEventListener("resize", updateDimensions);
+            return () => window.removeEventListener("resize", updateDimensions);
+        }, []);
+
     return(
         <div id='IdOne' className='one'>
-            <div>{Width1} <br/> {Width2} <br/> {Width3} <br/> </div>
+            <div style={{color:"red"}}>{width} <br/> {height}  </div>
 
             <div className='boxPicText'>
                 <img className='picSara' src={picSara} alt="" />
