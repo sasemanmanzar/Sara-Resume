4b2c1105086dc2ba3d440289d1dda5885f7efac6
Sara <sara@local>
2024-02-24 00:29:00 +0330

    transition

--- src/Pages/One/One.js
+++ src/Pages/One/One.js
@@ -141,8 +141,8 @@ function One(){
 
 
     return(
-        <div id='IdOne' className='one' onload={handleAnimation}>
-            <div style={{color:"red"}}>{width} <br/> {height} <br/> old= <br/> {previousWidthValue.current} <br/> {previousHeightValue.current}</div>
+        <div id='IdOne' className='one'>
+            {/* <div style={{color:"red"}}>{width} <br/> {height} <br/> old= <br/> {previousWidthValue.current} <br/> {previousHeightValue.current}</div> */}
 
             <div id="boxPicText" className='boxPicText'>
                 <img className='picSara' src={picSara} alt="" />
