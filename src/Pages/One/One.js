2e48612fb107bed0796e5fe1687df7bcf68e2924
Sara <sara@local>
2024-02-29 06:19:59 +0330

    ok

--- src/Pages/One/One.js
+++ src/Pages/One/One.js
@@ -81,9 +81,9 @@ function One(){
 
     return(
         <div id='IdOne' className='one'>
-            <div style={{color:"red"}}>{width} <br/> {height} <br/> old= <br/> {previousWidthValue.current} <br/> {previousHeightValue.current}</div>
+            {/* <div style={{color:"red"}}>{width} <br/> {height} <br/> old= <br/> {previousWidthValue.current} <br/> {previousHeightValue.current}</div> */}
 
-            <div id="boxPicText" className='boxPicText'>
+             <div id="boxPicText" className='boxPicText'>
                 <img className='picSara' src={picSara} alt="" />
                 <div className='textPageOne'>سارا آسمان منظر</div>
              </div>
