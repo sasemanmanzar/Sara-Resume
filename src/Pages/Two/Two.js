9e2f776f5ec770344ef35fabb1e1112dee90eef7
Sara <sara@local>
2024-03-03 01:16:44 +0330

    pg2

--- src/Pages/Two/Two.js
+++ src/Pages/Two/Two.js
@@ -1,21 +1,21 @@
 import './Two.css';
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
 
 function Two(){
 
-        const [width, setWidth]   = useState(window.innerWidth);
-        const [height, setHeight] = useState(window.innerHeight);
+        // const [width, setWidth]   = useState(window.innerWidth);
+        // const [height, setHeight] = useState(window.innerHeight);
         
-        const updateDimensions = () => {
-            setWidth(window.innerWidth);
-            setHeight(window.innerHeight);
-        }
+        // const updateDimensions = () => {
+        //     setWidth(window.innerWidth);
+        //     setHeight(window.innerHeight);
+        // }
 
-        useEffect(() => {
-            window.addEventListener("resize", updateDimensions);
-            return () => window.removeEventListener("resize", updateDimensions);
-        }, []);
+        // useEffect(() => {
+        //     window.addEventListener("resize", updateDimensions);
+        //     return () => window.removeEventListener("resize", updateDimensions);
+        // }, []);
 
 
 
@@ -23,9 +23,9 @@ function Two(){
         <div id='IdTwo' className='two'>
             {/* <div style={{color:"red"}}>{width} <br/> {height} </div> */}
             <div className='boxPage2'>
-            <div className='smallBox1 top1'><p>سوابق تحصیلی</p></div>
-            <div className='smallBox top2'><p>کارشناسی ارشد</p></div>
-            <div className='box top3'>
+            <div className='smallBox1'><p>سوابق تحصیلی</p></div>
+            <div className='smallBox'><p>کارشناسی ارشد</p></div>
+            <div className='box'>
                 <p>
                     <p id='subject'>مهندسی فناوری اطلاعات گرایش مدیریت سیستم های اطلاعاتی</p>
                     <p id='university'>موسسه آموزش عالی فناوری اطلاعات تعالی، قم ۱۳۹۴</p>
@@ -34,8 +34,8 @@ function Two(){
                     <p id='project'>پروژه پایانی: طراحی سیستم تصمیم یار پزشکی به منظور افزایش دقت با استفاده از تکنیک‌های داده کاوی (مورد واکاوی: مرکز شنوایی سنجی) / استاد راهنما: جناب آقای دکتر رضا احسن</p>
                 </p>
             </div>
-            <div className='smallBox top4'><p>کارشناسی</p></div>
-            <div className='box top5'>
+            <div className='smallBox'><p>کارشناسی</p></div>
+            <div className='box'>
                     <p id='subject'>مهندسی فناوری اطلاعات</p>
                     <p id='university'>دانشگاه صنعتی خواجه نصیر الدین طوسی، تهران ۱۳۹۱</p>
                     <p id='prise'>رتبه اول فارغ التحصیلان دوره کارشناسی رشته مهندسی فناوری اطلاعات</p>
