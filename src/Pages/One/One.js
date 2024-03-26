598f86a898bbb465c0f268f7a0d6826919910cd1
Sara <sara@local>
2024-03-26 16:43:40 +0330

    p1

--- src/Pages/One/One.js
+++ src/Pages/One/One.js
@@ -81,7 +81,7 @@ function One(){
 
     return(
         <div id='IdOne' className='one'>
-            <div style={{color:"red"}}>{width} <br/> {height} <br/> old= <br/> {previousWidthValue.current} <br/> {previousHeightValue.current}</div> 
+            {/* <div style={{color:"red"}}>{width} <br/> {height} <br/> old= <br/> {previousWidthValue.current} <br/> {previousHeightValue.current}</div>  */}
 
             <div className='boxAll'>
               <div id="boxPicText" className='boxPicText'>
