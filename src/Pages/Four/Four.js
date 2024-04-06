4149dafcc7dba5362a25a67b05a227f1216e43c8
Sara <sara@local>
2024-04-06 17:15:50 +0330

    pg4

--- src/Pages/Four/Four.js
+++ src/Pages/Four/Four.js
@@ -6,14 +6,116 @@ function Four(){
 
     return(
         <div id='IdFour' className='four'>
-            <div className='smallBoxSkill topSkill1'><p>مهارت ها</p></div>
-            <table className='tableP4'>
-                {/* <tr>
-                    <th>زبان خارجه</th>
-                    <th></th>
-                    <th>حوزه کامپیوتر</th>
-                </tr> */}
+            <div className='boxPage4'>
+            <div className='smallBoxSkill'><p>مهارت ها</p></div>
+
+            <div className='boxSkill'>
+
+                <div className='boxs'>
+                    <div>React.Js</div>
+                    <div id="ReactJs" className='value'></div>
+                </div>
+
+                <div className='boxs'>
+                    <div>Data Mining</div>
+                    <div id="DataMining" className='value'></div>
+                </div>
+
+                <div className='boxs'>
+                    <div>Android Development</div>
+                    <div id="AndroidDevelopment" className='value'></div>
+                </div>
+
+                <div className='boxs'>
+                    <div>Java Script</div>
+                    <div id="JavaScript" className='value'></div>
+                </div>
+
+                <div className='boxs'>
+                    <div>Front-end Development</div>
+                    <div id="FrontEnd" className='value'></div>
+                </div>
+
+                <div className='boxs'>
+                    <div>Cascading Style Sheets (CSS)</div>
+                    <div id="CSS" className='value'></div>
+                </div>
+
+                <div className='boxs'>
+                    <div>Research</div>
+                    <div id="Research" className='value'></div>
+                </div>
+
+                <div className='boxs'>
+                    <div>Persian language</div>
+                    <div id="PersianLanguage" className='value'></div>
+                </div>
+
+                <div className='boxs'>
+                    <div>English language</div>
+                    <div id="EnglishLanguage" className='value'></div>
+                </div>
+
+                <div className='boxs'>
+                    <div>German language</div>
+                    <div id="GermanLanguage" className='value'></div>
+                </div>
+            </div>
+
+
+            {/* <table className='tableP4'>
                 <tr>
+                    <td id="titleTP4">Data Mining</td>
+                    <td id="blank-middel"></td>
+                    <td id="titleTP4">React.Js</td>
+                </tr>
+                <tr>
+                    <td id="sk1"></td>
+                    <td id="blank-middel"></td>
+                    <td id="sk3"></td>
+                </tr>
+                <tr>
+                    <td id="titleTP4">Android Development</td>
+                    <td id="blank-middel"></td>
+                    <td id="titleTP4">Research</td>
+                </tr>
+                <tr>
+                    <td id="sk01"></td>
+                    <td id="blank-middel"></td>
+                    <td id="sk03"></td>
+                </tr>
+                <tr>
+                    <td id="titleTP4">Front-end Development</td>
+                    <td id="blank-middel"></td>
+                    <td id="titleTP4">Persian language</td>
+                </tr>
+                <tr>
+                    <td id="sk001"></td>
+                    <td id="blank-middel"></td>
+                    <td id="sk003"></td>
+                </tr>
+                <tr>
+                    <td id="titleTP4">Cascading Style Sheets (CSS)</td>
+                    <td id="blank-middel"></td>
+                    <td id="titleTP4">English language</td>
+                </tr>
+                <tr>
+                    <td id="sk0001"></td>
+                    <td id="blank-middel"></td>
+                    <td id="sk0003"></td>
+                </tr>
+                <tr>
+                    <td id="titleTP4">Java Script</td>
+                    <td id="blank-middel"></td>
+                    <td id="titleTP4">German language</td>
+                </tr>
+                <tr>
+                    <td id="sk00001"></td>
+                    <td id="blank-middel"></td>
+                    <td id="sk00003"></td>
+                </tr> */}
+
+                {/* <tr>
                     <td id="s1">Data Mining</td>
                     <td id="s2"></td>
                     <td id="s3">React.Js</td>
@@ -62,8 +164,9 @@ function Four(){
                     <td id="sk00001"></td>
                     <td id="sk00002"></td>
                     <td id="sk00003"></td>
-                </tr>
-            </table>
+                </tr> */}
+            {/* </table> */}
+        </div>
         </div>
     );
 }
