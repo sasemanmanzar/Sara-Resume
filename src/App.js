d14c13f2545b145cb1045ee04416c39928e213f5
Sara <sara@local>
2024-04-19 22:13:23 +0330

    circle app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Four from './Pages/Four/Four';
 import Five from './Pages/Five/Five';
 import Six from './Pages/Six/Six';
 
+import { useState } from 'react';
+
 // import Test from './Pages/test';
 
 function App() {
@@ -13,11 +15,58 @@ function App() {
     // const button = () => {
     //   alert("Hi!");
     // }
+  
+  // const backgroundDot1 = document.getElementById();
+  
+  // const [backgroundDot1 , setBackgroundDot1] = useState('gold');
+  // const [backgroundDot2 , setBackgroundDot2] = useState('gold');
+  // const [backgroundDot3 , setBackgroundDot3] = useState('gold');
+  // const [backgroundDot4 , setBackgroundDot4] = useState('gold');
+  // const [backgroundDot5 , setBackgroundDot5] = useState('gold');
+  // const [backgroundDot6 , setBackgroundDot6] = useState('gold');
+
+  // const [backgroundDot , setBackgroundDot] = useState({
+  //   dot1: "gold",
+  //   dot2: "gold",
+  //   dot3: "gold",
+  //   dot4: "gold",
+  //   dot5: "gold",
+  //   dot6: "gold"
+  // });
+
+  // const [activeDot , setActiveDot] = useState(false);
+
+  const [activeDot , setActiveDot] = useState({
+    dot1: false,
+    dot2: false,
+    dot3: false,
+    dot4: false,
+    dot5: false,
+    dot6: false
+  });
+
+
+  // const Dot6 = document.getElementById('dot6');
+
 
   const scrollToMarginPage1 = () => {
     const marginDot1 = document.getElementById('IdOne'); 
       if (marginDot1) {
         marginDot1.scrollIntoView();
+        // setBackgroundDot.dot1("red");
+        // setBackgroundDot.dot2("gold");
+        
+        // setBackgroundDot(previousState => {
+        //   return { ...previousState, dot1: "blue" }
+        // });
+
+        // setBackgroundDot(() => {
+        //   return { dot1: "blue" , dot2:'gold' , dot3:'gold', dot4:'gold', dot5:'gold', dot6:'gold' }
+        // });
+        
+        setActiveDot(() => {
+          return { dot1: true, dot2: false, dot3: false, dot4:false, dot5:false, dot6:false }
+        });
       }
   };
 
@@ -25,6 +74,18 @@ function App() {
     const marginDot2 = document.getElementById('IdTwo'); 
       if (marginDot2) {
         marginDot2.scrollIntoView();
+        // setBackgroundDot2("red");
+        // setBackgroundDot(previousState => {
+        //   return { ...previousState, dot2: "blue" }
+        // });
+        // setBackgroundDot(() => {
+        //   return { dot1: "gold" , dot2:'blue' , dot3:'gold', dot4:'gold', dot5:'gold', dot6:'gold' }
+        // });
+        // setActiveDot(true);
+        
+        setActiveDot(() => {
+            return { dot1: false, dot2: true, dot3: false, dot4:false, dot5:false, dot6:false }
+          });
       }
   };
 
@@ -33,6 +94,14 @@ function App() {
     const marginDot3 = document.getElementById('IdThree'); 
       if (marginDot3) {
         marginDot3.scrollIntoView();
+        // setBackgroundDot3("red");
+        // setBackgroundDot(() => {
+        //   return { dot1: "gold" , dot2:'gold' , dot3:'blue', dot4:'gold', dot5:'gold', dot6:'gold' }
+        // });
+        // setActiveDot(true);
+        setActiveDot(() => {
+          return { dot1: false, dot2: false, dot3: true, dot4:false, dot5:false, dot6:false }
+        });
       }
   };
 
@@ -40,6 +109,14 @@ function App() {
     const marginDot4 = document.getElementById('IdFour'); 
       if (marginDot4) {
         marginDot4.scrollIntoView();
+        // setBackgroundDot4("red");
+        // setBackgroundDot(() => {
+        //   return { dot1: "gold" , dot2:'gold' , dot3:'gold', dot4:'blue', dot5:'gold', dot6:'gold' }
+        // });
+        // setActiveDot(true);
+        setActiveDot(() => {
+          return { dot1: false, dot2: false, dot3: false, dot4:true, dot5:false, dot6:false }
+        });
       }
   };
 
@@ -47,6 +124,14 @@ function App() {
     const marginDot5 = document.getElementById('IdFive'); 
       if (marginDot5) {
         marginDot5.scrollIntoView();
+        // setBackgroundDot5("red");
+        // setBackgroundDot(() => {
+        //   return { dot1: "gold" , dot2:'gold' , dot3:'gold', dot4:'gold', dot5:'blue', dot6:'gold' }
+        // });
+        // setActiveDot(true);
+        setActiveDot(() => {
+          return { dot1: false, dot2: false, dot3: false, dot4:false, dot5:true, dot6:false }
+        });
       }
   };
 
@@ -54,6 +139,15 @@ function App() {
     const marginDot6 = document.getElementById('IdSix'); 
       if (marginDot6) {
         marginDot6.scrollIntoView();
+        // setBackgroundDot6("red");
+        // setBackgroundDot(() => {
+        //   return { dot1: "gold" , dot2:'golg' , dot3:'gold', dot4:'gold', dot5:'gold', dot6:'blue' }
+        // });
+        // Dot6.backgroundColor('red');
+        // setActiveDot(true);
+        setActiveDot(() => {
+          return { dot1: false, dot2: false, dot3: false, dot4:false, dot5:false, dot6:true }
+        });
       }
   };
 
@@ -65,12 +159,12 @@ function App() {
   return (
     <div>
       <div>    
-        <div className='circle dot1' onClick={scrollToMarginPage1} />
-        <div className='circle dot2' onClick={scrollToMarginPage2} />
-        <div className='circle dot3' onClick={scrollToMarginPage3} />
-        <div className='circle dot4' onClick={scrollToMarginPage4} />
-        <div className='circle dot5' onClick={scrollToMarginPage5} />
-        <div className='circle dot6' onClick={scrollToMarginPage6} />
+        <div className='circle dot1' onClick={scrollToMarginPage1} style={{backgroundColor: activeDot.dot1? 'red' : 'gold' }}/>
+        <div className='circle dot2' onClick={scrollToMarginPage2} style={{backgroundColor: activeDot.dot2? 'red' : 'gold' }}/>
+        <div className='circle dot3' onClick={scrollToMarginPage3} style={{backgroundColor: activeDot.dot3? 'red' : 'gold' }}/>
+        <div className='circle dot4' onClick={scrollToMarginPage4} style={{backgroundColor: activeDot.dot4? 'red' : 'gold' }}/>
+        <div className='circle dot5' onClick={scrollToMarginPage5} style={{backgroundColor: activeDot.dot5? 'red' : 'gold' }}/>
+        <div className='circle dot6' onClick={scrollToMarginPage6} style={{backgroundColor: activeDot.dot6? 'red' : 'gold' }} />
       </div>
 
       {/* <article className='scroller'>
@@ -82,6 +176,15 @@ function App() {
         <section><Six/></section>
       </article> */}
 
+
+      {/* <div> 
+            1: {backgroundDot.dot1} <br/>
+            2: {backgroundDot.dot2} <br/>
+            3: {backgroundDot.dot3} <br/> 
+            4: {backgroundDot.dot4} <br/>
+            5: {backgroundDot.dot5} <br/>
+            6: {backgroundDot.dot6} <br/>
+      </div> */}
 
       <div className='scroller'>
         <div className='section'><One/></div>
